232c2ee1321a7cba111aa34cb6c7dbb1e41a33c4
Angelica Simarra <simarra.angelica@gmail.com>
2025-09-05 11:41:00 -0500

    feat: send email token with nodemailer for user registration flow

--- src/user/UserService.js
+++ src/user/UserService.js
@@ -1,18 +1,22 @@
 const crypto = require('node:crypto');
 const bcryp = require('bcrypt');
 const User = require('./User');
+const EmailService = require('../../email/EmailService');
 
 const save = async (userData) => {
   const { username, email, password } = userData;
 
   const hashedPassword = await bcryp.hash(password, 10);
+  const activationToken = _generateToken();
 
   await User.create({
     username,
     email,
     password: hashedPassword,
-    activationToken: _generateToken(),
+    activationToken,
   });
+
+  return EmailService.sendAccountActivation(email, activationToken);
 };
 
 const findByEmail = async (email) => {
