af8779bc6a7eddd8cb554bbb6f95eaf0b4ac52a6
Angelica Simarra <simarra.angelica@gmail.com>
2025-09-09 09:01:48 -0500

    feat: configure email transport settings and enhance account activation email format

--- email/EmailService.js
+++ email/EmailService.js
@@ -1,12 +1,24 @@
+const nodemailer = require('nodemailer');
 const transporter = require('../src/config/emailTransporter');
 
 const sendAccountActivation = async (email, token) => {
-  return await transporter.sendMail({
+  const response = await transporter.sendMail({
     from: 'My App <info@my-app.com>',
     to: email,
     subject: 'Account Activation',
-    html: `Token is ${token}`,
+    html: `
+      <b>Please click below link to activate your account</b>
+      <br/>
+      <a href="http://localhost:8080/#/login?token=${token}">Activate</a>
+      Token is ${token}
+    `,
   });
+
+  if (process.env.NODE_ENV === 'development') {
+    console.log('url: ' + nodemailer.getTestMessageUrl(response));
+  }
+
+  return response;
 };
 
 module.exports = { sendAccountActivation };
